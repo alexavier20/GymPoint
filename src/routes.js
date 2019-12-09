7a38ecc7f315ca9e00d08dedf1676cbc1cb03ab2
alexavier20 <alex_xavier@outlook.com>
2019-12-09 11:18:36 -0300

    Desafio 02

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import StudentController from './app/controllers/StudentController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -11,5 +12,8 @@ routes.post('/users', UserController.store);
 routes.put('/users', authMiddleware, UserController.update);
 
 routes.post('/sessions', SessionController.store);
+
+routes.post('/students', authMiddleware, StudentController.store);
+routes.put('/students/:id', authMiddleware, StudentController.update);
 
 export default routes;
